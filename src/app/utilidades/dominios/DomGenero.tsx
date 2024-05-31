bd74df3f9c91d1466affad1ee54f66f686acc06d
Juan Francisco <jperez201@cuc.edu.co>
2024-05-30 22:10:29 -0500

    Primera_vercion

---
+++ src/app/utilidades/dominios/DomGenero.tsx
@@ -0,0 +1,9 @@
+import { CamisaGenero } from "../../modelos/CamisaGenero";
+
+export const ARREGLO_CAMISA_TALLA: Array<CamisaGenero> = [
+  { codGenero: "1", nombreGenero: "S" },
+  { codGenero: "2", nombreGenero: "M" },
+  { codGenero: "3", nombreGenero: "X" },
+  { codGenero: "4", nombreGenero: "XL" },
+  { codGenero: "5", nombreGenero: "XXL" },
+];
